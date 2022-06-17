4aba28f72c71fbef5fd01cc5159b8029f37a0926
carlinhoshk <carlosmdohk@gmail.com>
2022-06-17 01:06:53 -0300

    Possivel mudança para criar um while para os encod

---
+++ Main/while.py
@@ -0,0 +1,12 @@
+a=0
+
+while True:
+    a = a + 1
+    print("carometro_legislatura56/image-%003d.jpg" % a)
+    if a == 20:
+        
+        break # Isso acaba a iteração
+        print('Peso invalido.')
+        
+    print(a)
+
