8165705bc6cf2f16366aaa3823b44db0512c764f
carlinhoshk <carlosmdohk@gmail.com>
2022-06-18 20:04:36 -0300

    Criando uma função para encodar imagem dps array

---
+++ Main/TESTEVIDEO.py
@@ -0,0 +1,5 @@
+    
+    face_recognition("carometro_legislatura56/image-%003d.jpg" % a)
+    face_encoding(face_recognition)[0]
+    print(know_face_encodings)
+    known_face_encodings = [face_encoding]
